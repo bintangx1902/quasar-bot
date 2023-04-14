62368a7c475141b92ea9be63df7b56594a62aeb4
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-04-14 08:46:57 +0700

    testing

--- testing.py
+++ testing.py
@@ -1,42 +1,59 @@
 import cv2
-import numpy as np
-from tensorflow import keras
+import tensorflow as tf
 import keras
+import numpy as np
 
-# Load the trained model
-model = keras.models.load_model('./model/v1.h5')
+# Load your TensorFlow machine learning model
+model = tf.keras.models.load_model(r'C:\Users\binta\Desktop\raw\model\SAR-terrain-v1.5.h5')
 
-# Open video capture
-cap = cv2.VideoCapture(0)  # Use camera index 0 for the default camera
+# Open a video capture object
+cap = cv2.VideoCapture('./dataset/train2.mp4')  # 0 for default camera, or provide the path to a video file
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # Set camera resolution to 1280x720
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+labels = ['RETAK', 'TANGGA']
+
+
+def postprocess_outputs(predictions):
+    box_x, box_y, box_w, box_h, class_index = None, None, None, None, np.argmax(predictions)
+    return box_x, box_y, box_w, box_h, class_index
+
+
+def draw_box(frame, box_x, box_y, box_w, box_h, class_label):
+    f_copy = frame.copy()
+    if box_x is not None and box_y is not None and box_w is not None and box_h is not None:
+        cv2.rectangle(f_copy, (box_x, box_y), (box_x + box_w, box_y + box_h), (0, 255, 0), 2)
+        if class_label is not None:
+            cv2.putText(f_copy, class_label, (box_x, box_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    return f_copy
+
+
+# Loop over video frames
 while True:
-    # Capture frame from video
     ret, frame = cap.read()
 
-    # Preprocess frame
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
-    frame = cv2.resize(frame, (192, 255))  # Resize to match input shape of the model
-    frame = frame / 127.5 - 1
-    frame = np.expand_dims(frame, axis=0)  # Add batch dimension
-    frame = np.vstack([frame])
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (192, 255))
+    frame_normalized = frame_resized / 255.0
+    frame_expanded = tf.expand_dims(frame_normalized, axis=0)
+    predictions = model.predict(frame_expanded)
+    print(predictions)
+    box_x, box_y, box_w, box_h, class_index = postprocess_outputs(predictions)
 
-    # Make prediction
-    predictions = model.predict(frame)
+    class_label = None
+    if class_index is not None:
+        class_label = labels[class_index]
 
-    # Extract class and confidence from prediction
-    class_index = np.argmax(predictions)
-    confidence = predictions[0][class_index]
+    # Draw bounding box and class label on video frame
+    frame_with_box = draw_box(frame, box_x, box_y, box_w, box_h, class_label)
 
-    # Draw bounding box around detected object
-    cv2.rectangle(frame, (0, 0), (255, 192), (255, 0, 0), 2)  # Example bounding box, adjust as needed
+    cv2.imshow('Video', frame_with_box)
 
-    # Display frame
-    cv2.imshow('Object Detection', frame)
+    if class_label is not None:
+        print(class_label)
 
-    # Exit loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release video capture and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
