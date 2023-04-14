d242b8bd413b7e8e01da8e94b0ecbf46d0932351
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-04-14 08:46:34 +0700

    the model has been combined but not coded yet

--- robot.py
+++ robot.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-
+from machine import *
 from movement import *
 import time
+import tensorflow as tf
 
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(r'./dataset/train2.mp4')
@@ -13,6 +14,9 @@ if cap.isOpened():
     fps = cap.get(cv2.CAP_PROP_FPS)
 
 thresh = .6
+
+model = tf.keras.models.load_model(r'C:\Users\binta\Desktop\raw\model\SAR-terrain-v1.5.h5')
+labels = ['RETAK', 'TANGGA']
 
 while True:
     # _, frame = cap.read()
@@ -25,7 +29,21 @@ while True:
         break
 
     conv = frame_conv_opt(binary, 2, 3)
-    print(conv)
+    # print(conv)
+
+    """ machine learning """
+    frame_resized = cv2.resize(frame, (192, 255))
+    frame_normalized = frame_resized / 255.0
+    frame_expanded = tf.expand_dims(frame_normalized, axis=0)
+    predictions = model.predict(frame_expanded)
+
+    box_x, box_y, box_w, box_h, class_index = postprocess_outputs(predictions, np)
+
+    class_label = None
+    if class_index is not None:
+        class_label = labels[class_index]
+
+    frame_with_box = draw_box(frame, box_x, box_y, box_w, box_h, class_label, cv2)
 
     if ((conv[1, :2] < thresh).all() and conv[0, :1] < thresh) or (conv[0, :1] < thresh and conv[1, :1] < thresh):
         """ move left will be ignored if the condition of angled left acceptable """
@@ -66,7 +84,7 @@ while True:
     # else:
     #     stop()
 
-    time.sleep(.05)
+    # time.sleep(.05)
 
     line_y = int(height // 2)
 
@@ -78,7 +96,11 @@ while True:
     cv2.line(binary, (line1_x, 0), (line1_x, height), (0, 255, 0), 2)
     cv2.line(binary, (line2_x, 0), (line2_x, height), (0, 255, 0), 2)
 
-    cv2.imshow("Real Footage", frame)
+    cv2.imshow('Video', frame_with_box)
+
+    if class_label is not None:
+        print(class_label)
+
     cv2.imshow("Binary Footage", binary)
 
     if cv2.waitKey(1) == ord('q') or cv2.waitKey(1) == ord('c'):
