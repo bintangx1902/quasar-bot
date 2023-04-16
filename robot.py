0693b1a4c04cbbce79e6037b228fd285db868fea
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-04-17 00:33:23 +0700

    robot movement need to be check with the hardware

--- robot.py
+++ robot.py
@@ -4,6 +4,8 @@ from machine import *
 from movement import *
 import time
 import tensorflow as tf
+from tensorflow import keras
+import keras
 
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(r'./dataset/train2.mp4')
@@ -15,8 +17,8 @@ if cap.isOpened():
 
 thresh = .6
 
-model = tf.keras.models.load_model(r'C:\Users\binta\Desktop\raw\model\SAR-terrain-v1.5.h5')
-labels = ['RETAK', 'TANGGA']
+model = keras.models.load_model(r'C:\Users\binta\Desktop\raw\model\SAR-terrain-v2.h5')
+labels = ['GOOD', 'RETAK', 'TANGGA']
 
 while True:
     # _, frame = cap.read()
@@ -40,23 +42,37 @@ while True:
     box_x, box_y, box_w, box_h, class_index = postprocess_outputs(predictions, np)
 
     class_label = None
-    if class_index is not None:
-        class_label = labels[class_index]
+    class_label = labels[class_index] if class_label is not None else None
 
     frame_with_box = draw_box(frame, box_x, box_y, box_w, box_h, class_label, cv2)
 
-    if ((conv[1, :2] < thresh).all() and conv[0, :1] < thresh) or (conv[0, :1] < thresh and conv[1, :1] < thresh):
-        """ move left will be ignored if the condition of angled left acceptable """
-        if ((conv[0, :2] < thresh).all() and (conv[1, :] < thresh).all()) or \
-                (conv[0, :1] < thresh and (conv[1, :] < thresh).all):
-            move_angled_left()
-        else:
-            move_left()
-    elif ((conv[0, :] < thresh).all() and (conv[1, :] < thresh).all()) or \
-            ((conv[1, :]).all() and np.array_equal(conv[0, :], [False, True, False])) or \
-            (conv[1, :] < thresh).all() or ((conv[0, :] < thresh)[1] and (conv[1, :] < thresh)[1]) or \
-            (conv[1, :] < thresh)[1]:
+    if class_label in labels:
         move_forward()
+        if ((conv[1, :2] < thresh).all() and conv[0, :1] < thresh) or (conv[0, :1] < thresh and conv[1, :1] < thresh):
+            """ move left will be ignored if the condition of angled left acceptable """
+            if ((conv[0, :2] < thresh).all() and (conv[1, :] < thresh).all()) or \
+                    (conv[0, :1] < thresh and (conv[1, :] < thresh).all):
+                move_angled_left()
+            else:
+                move_left()
+        elif (np.array_equal(conv[0, :] < thresh, [False, False, True]) and
+              np.array_equal(conv[1, :] < thresh, [False, True, True])) \
+                or (np.array_equal(conv[0, :] < thresh, [False, False, True]) or
+                    np.array_equal(conv[1, :] < thresh, [False, False, True])):
+            if ((conv[0, 1:3] < thresh).all() and (conv[1, :] < thresh).all()) or \
+                    (conv[0, 2:3] < thresh or (conv[1, :] < thresh).all()) or \
+                    (conv[0, 1:3] < thresh).all() or (conv[1, 1:3]).all():
+                move_angled_right()
+            else:
+                move_right()
+
+        elif ((conv[0, :] < thresh).all() and (conv[1, :] < thresh).all()) or \
+                ((conv[1, :]).all() and np.array_equal(conv[0, :], [False, True, False])) or \
+                (conv[1, :] < thresh).all() or ((conv[0, :] < thresh)[1] and (conv[1, :] < thresh)[1]) or \
+                (conv[1, :] < thresh)[1]:
+            move_forward()
+        else:
+            stop()
 
     # if (conv[1, :] < THRESH).all() and (conv[0, :] < THRESH)[1]:
     #     move_forward()
@@ -96,11 +112,12 @@ while True:
     cv2.line(binary, (line1_x, 0), (line1_x, height), (0, 255, 0), 2)
     cv2.line(binary, (line2_x, 0), (line2_x, height), (0, 255, 0), 2)
 
-    cv2.imshow('Video', frame_with_box)
+    # cv2.imshow('Video', frame_with_box)
+    #
+    # if class_label is not None:
+    #     print(class_label)
 
-    if class_label is not None:
-        print(class_label)
-
+    cv2.imshow("Real Footage", frame)
     cv2.imshow("Binary Footage", binary)
 
     if cv2.waitKey(1) == ord('q') or cv2.waitKey(1) == ord('c'):
